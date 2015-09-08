d143b327cccaacf9bb55dbe2318ead8e7381d3e5
Lance Kindle <lance.kindle@gmail.com>
2015-09-08 10:24:33 -0700

    adjust sys.path so that main.py can be imported

--- hooks/gitta_hook.py
+++ hooks/gitta_hook.py
@@ -7,7 +7,7 @@ def trigger(*args, **kwargs):
     git_hooks_dir, event = os.path.split(hook_path)  # e.g. '.git/hooks',   'post-checkout'
     git_dir_relative, _ = os.path.split(git_hooks_dir)  # e.g. '.git'
     git_dir = os.path.join(repo_dir, git_dir_relative)
-    path_to_gitta_pyfiles = os.path.join(repo_dir, 'gitTA')  # EVENTUALLY I want to run only code within .git
+    path_to_gitta_pyfiles = os.path.join(repo_dir)  # EVENTUALLY I want to run only code within .git
     # so EVENTUALLY all gitTA files will be in .git/gitTA  (including hooks and py folders)
     sys.path.append(path_to_gitta_pyfiles)    # add path so that importing main and gitta works
     import main
