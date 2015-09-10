afe203a0ed864d9e545373f1f483ee321726fe82
Lance Kindle <lance.kindle@gmail.com>
2015-09-10 10:53:20 -0700

    list hook events more presentably

--- main.py
+++ main.py
@@ -6,15 +6,21 @@ import colorama
 from colorama import Fore, Back  # add color output to terminal: we want anything printed to be VERY visible to user
 colorama.init()  # called so that windows colors work
 '''
-modify this file! When git runs certain commands, it will run THIS main.py
+MODIFY THIS FILE! When git is run with certain commands, it also triggers hooks
+called git hooks. These hooks are tied in to directly run THIS main.py
 which will trigger the functions you've decorated here with gitta.listen('event-name')
 your methods can listen for the following events:
-pre-push, pre-commit,  # pre-x methods can be aborted by raising an exception
-post-commit, post-checkout, post-merge
+CHECKOUT: post-checkout
+COMMIT:  pre-commit*, prepare-commit-msg*, commit-msg*, post-commit
+PUSH: pre-push*
+MERGE: post-merge
+... and even more: pre-applypatch*, applypatch-msg*, post-applypatch,
+post-rebase, pre-auto-gc*
+You can abort git actions marked with * by raising an error in your listening method. e.g. you can abort a commit by raising an error in any of the three commit hooks.
+I strongly suggest you search 'git hook tutorial' online to determine which hook you most want. https://www.atlassian.com/git/tutorials/git-hooks/local-hooks
 '''
 git = gitTA.gitta()
 
-# pre-* events can be aborted by raising an exception ???
 @git.listen('pre-push')
 def prepush(*args, **kwargs):
     print(Fore.GREEN)  # set so that ALL next prints will be green
@@ -32,15 +38,17 @@ def postcommit(*args, **kwargs):
 
 @git.listen('post-checkout')
 def postcheckout(*args, **kwargs):
-    print(Fore.GREEN)  # set so that ALL next prints will be green
+    print(Fore.GREEN)
     print(args, kwargs)
 
 @git.listen('post-merge')
 def postmerge(*args, **kwargs):
+    print(Fore.GREEN)
     print(args, kwargs)
 
 if __name__ == '__main__':
-    gitTA.gitta.trigger_all_instances(45, event='post-checkout')  # example of what might get passed to postcheckout
+    gitTA.gitta.trigger_all_instances(45, event='post-checkout') 
+    # example of what might get passed to postcheckout
     # the garbled message that appears before (45, ) is the Fore.GREEN. On normal terminals this garbled output will NOT appear
     
 
